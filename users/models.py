7def1ecc5625f47c2e0b7617268e7bfdd0bd2c9a
Artem Zhitkov <aptem_deadly@mail.ru>
2025-04-02 19:40:02 +0500

    black

--- users/models.py
+++ users/models.py
@@ -4,10 +4,19 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, verbose_name="Email", help_text="Введите Email")
-    city = models.CharField(max_length=20, verbose_name="Телефон", help_text="Введите номер телефона", blank=True,
-                            null=True)
-    avatar = models.ImageField(upload_to="users/avatars", verbose_name="Аватар", blank=True, null=True)
+    email = models.EmailField(
+        unique=True, verbose_name="Email", help_text="Введите Email"
+    )
+    city = models.CharField(
+        max_length=20,
+        verbose_name="Телефон",
+        help_text="Введите номер телефона",
+        blank=True,
+        null=True,
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars", verbose_name="Аватар", blank=True, null=True
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
