362e9254df3a714489cc5d7fbc9bc007fa65ceb9
Artem Zhitkov <aptem_deadly@mail.ru>
2025-04-05 17:01:19 +0500

    register User

--- habit_tracker/admin.py
+++ habit_tracker/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from .models import Habit
 
+
 @admin.register(Habit)
 class HabitAdmin(admin.ModelAdmin):
-    list_display = "__all__"
+    list_display = (
+        "id",
+        "user",
+        "time",
+        "place",
+        "action",
+        "pleasant_habit",
+        "related_habit",
+        "periodicity",
+        "reward",
+        "time_to_complete",
+        "is_published",
+    )
