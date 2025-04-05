84cc0bbc1f7e2c816cc18c7ce7a737fae89c372f
Artem Zhitkov <aptem_deadly@mail.ru>
2025-04-05 10:56:20 +0500

    add urls

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 
 from habit_tracker.apps import HabitTrackerConfig
+from habit_tracker.views import (
+    HabitCreateAPIView,
+    HabitListAPIView,
+    HabitRetrieveAPIView,
+    HabitUpdateAPIView,
+    HabitDestroyAPIView,
+)
 
 app_name = HabitTrackerConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path("create/", HabitCreateAPIView.as_view(), name="create-habit"),
+    path("list/", HabitListAPIView.as_view(), name="habit-list"),
+    path("<int:pk>/", HabitRetrieveAPIView.as_view(), name="habit-detail"),
+    path("<int:pk>/update/", HabitUpdateAPIView.as_view(), name="habit-update"),
+    path("<int:pk>/delete/", HabitDestroyAPIView.as_view(), name="habit-delete"),
+]
