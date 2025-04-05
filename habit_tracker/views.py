4a521c82f84af15d179d1c6df81f1e0d5338fb09
Artem Zhitkov <aptem_deadly@mail.ru>
2025-04-05 10:55:57 +0500

    add endpoint

--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from habit_tracker.models import Habit
+from habit_tracker.serializers import HabitSerializer
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    """Эндпоинт создания привычки"""
+
+    serializer_class = HabitSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class HabitListAPIView(generics.ListAPIView):
+    """Эндпоинт списка привычек"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    """Эндпоинт просмотра привычки"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    """Эндпоинт изменения привычки"""
+
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    """Эндпоинт удаления привычки"""
+
+    queryset = Habit.objects.all()
