1caaa4a0a7389417384bbcbb77b06c901904e8be
Artem Zhitkov <aptem_deadly@mail.ru>
2025-04-05 10:59:28 +0500

    register "Habit"

--- habit_tracker/admin.py
+++ habit_tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Habit
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = "__all__"
