463f73479ef5cd39de3cca3b3de930fd1583e415
Artem Zhitkov <aptem_deadly@mail.ru>
2025-04-06 22:14:33 +0500

    add urls

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.apps import UsersConfig
-
+from users.views import UserCreateAPIView
 
 app_name = UsersConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", UserCreateAPIView.as_view(), name="register-user"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
